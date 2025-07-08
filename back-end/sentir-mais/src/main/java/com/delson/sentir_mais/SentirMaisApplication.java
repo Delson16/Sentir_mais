c2984758433f64980bdf4c922ac816c9ded620d3
Delson Pilar <146981737+Delson16@users.noreply.github.com>
2025-07-08 18:43:58 -0300

    Adicionando API iniciada no Spring Initializer

---
+++ back-end/sentir-mais/src/main/java/com/delson/sentir_mais/SentirMaisApplication.java
@@ -0,0 +1,13 @@
+package com.delson.sentir_mais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentirMaisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SentirMaisApplication.class, args);
+	}
+
+}
